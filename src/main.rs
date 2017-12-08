e1f8e6c34962d272da48e22c6b3c5bf5f389ec69
k-h-m <misha.kharlamov@gmail.com>
2017-12-08 12:21:49 +0300

    provide generator interface via `Gen` trait

--- src/main.rs
+++ src/main.rs
@@ -5,51 +5,117 @@ use std::f32::consts::PI;
 use std::i16;
 use clap::{App, Arg, SubCommand};
 
+
+trait Gen {
+    fn new(ampl: f32, freq: f32, phase: f32) -> Self;
+    fn gen(&self, x: f32) -> i16;
+}
+
+struct Sine {
+    ampl: f32,
+    freq: f32,
+    phase: f32
+}
+
+impl Gen for Sine {
+    fn new(ampl: f32, freq: f32, phase: f32) -> Sine {
+        Sine {ampl, freq, phase}
+    }
+
+    fn gen(&self, x: f32) -> i16 {
+        let arg = 2.0 * PI * (self.freq * x + self.phase / 360.0);
+        (self.ampl * arg.sin()) as i16
+    }
+}
+
+struct Square {
+    ampl: f32,
+    freq: f32,
+    phase: f32
+}
+
+impl Gen for Square {
+    fn new(ampl: f32, freq: f32, phase: f32) -> Square {
+        Square {ampl, freq, phase}
+    }
+
+    fn gen(&self, x: f32) -> i16 {
+        let arg = self.freq * x + self.phase / 360.0;
+        if arg.fract() < 0.5 {self.ampl as i16} else {-self.ampl as i16}
+    }
+}
+
+struct Silence {}
+
+impl Gen for Silence {
+    fn new(_ampl: f32, _freq: f32, _phase: f32) -> Silence {
+        Silence {}
+    }
+
+    fn gen(&self, _x: f32) -> i16 {
+        0
+    }
+}
+
+struct Ticks<T: Gen> {
+    curr_tick: u32,
+    last_tick: u32,
+    step: f32,
+    generator: T
+}
+
+impl<T: Gen> Ticks<T> {
+    fn new(duration: f32, sample_rate: f32, generator: T) -> Ticks<T> {
+        assert!(duration >= 0.0);
+        assert!(sample_rate > 0.0);
+        Ticks { curr_tick: 0,
+                last_tick: (duration * sample_rate) as u32,
+                step: 1.0/sample_rate,
+                generator
+        }
+    }
+}
+
+impl<T: Gen> Iterator for Ticks<T> {
+    type Item = i16;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let tick = self.curr_tick;
+        if tick >= self.last_tick {return None}
+        self.curr_tick += 1;
+        Some(self.generator.gen(self.step * (tick as f32)))
+    }
+}
+
 const WAV_SPEC: hound::WavSpec = hound::WavSpec {
     channels: 2,
     sample_rate: 44100,
     bits_per_sample: 16,
-    sample_format: hound::SampleFormat::Int };
+    sample_format: hound::SampleFormat::Int
+};
 
-fn silence(_ampl:f32, _freq:f32, _phase:f32, _x:f32) -> f32 {
-  0.0
-}
-
-fn square(ampl:f32, freq:f32, phase:f32, x:f32) -> f32 {
-  let arg = freq*x + phase/360.0;
-  if arg.fract() < 0.5 {ampl} else {-ampl}
-}
-
-fn sine(ampl:f32, freq:f32, phase:f32, x:f32) -> f32 {
-  ampl*(2.0*PI*(freq*x + phase/360.0)).sin()
-}
-
-fn generate_plain<T: Fn(f32,f32,f32,f32) -> f32>(file:&str, freq:f32, dur:u32, phase:f32, shape:T) -> () {
-  let num_samples = WAV_SPEC.sample_rate * dur;
+fn wrr<T: Gen>(writer: &mut hound::WavWriter<std::io::BufWriter<std::fs::File>>, dur: f32, freq: f32, phase1: f32, phase2: f32) -> () {
   let ampl = i16::MAX as f32;
-  let mut writer = hound::WavWriter::create(file, WAV_SPEC).unwrap();
-  for n in 0 .. num_samples {
-    let t = n as f32 / WAV_SPEC.sample_rate as f32;
-    let left_chan = shape(ampl, freq, 0.0, t) as i16;
-    let right_chan = shape(ampl, freq, phase, t) as i16;
-    writer.write_sample(left_chan).unwrap();
-    writer.write_sample(right_chan).unwrap();
+  let rate = WAV_SPEC.sample_rate as f32;
+  let left = Ticks::new(dur, rate, T::new(ampl, freq, phase1));
+  let right = Ticks::new(dur, rate, T::new(ampl, freq, phase2));
+  for (l,r) in left.zip(right) {
+      writer.write_sample(l).unwrap();
+      writer.write_sample(r).unwrap();
   }
 }
 
-fn generate_combo<T: Fn(f32,f32,f32,f32) -> f32>(file:&str, freq:f32, dur:u32, sil:u32, phase:u32, shape:T) -> () {
-  let period = dur + sil; 
-  let num_samples = WAV_SPEC.sample_rate * period * (1 + 360 / phase);
-  let ampl = i16::MAX as f32;
+fn write_plain<T: Gen>(file: &str, dur: f32, freq: f32, phase: f32) -> () {
   let mut writer = hound::WavWriter::create(file, WAV_SPEC).unwrap();
-  for n in 0 .. num_samples {
-    let k = n / WAV_SPEC.sample_rate / period;
-    let t = n as f32 / WAV_SPEC.sample_rate as f32;
-    let p = (k * phase) as f32;
-    let (left_chan, right_chan) = if t < ((k * period + dur) as f32) {(shape(ampl, freq, 0.0, t), shape(ampl, freq, p, t))} 
-                                  else {(silence(ampl, freq, 0.0, t), silence(ampl, freq, p, t))};
-    writer.write_sample(left_chan as i16).unwrap();
-    writer.write_sample(right_chan as i16).unwrap();
+  wrr::<T>(&mut writer, dur, freq, 0.0, phase);
+}
+
+fn write_combo<T1: Gen, T2: Gen>(file:&str, dur1: f32, dur2: f32, freq:f32, shift: f32) -> () {
+  assert!(shift > 0.0);
+  let mut writer = hound::WavWriter::create(file, WAV_SPEC).unwrap();
+  for n in 0 .. (360.0/shift) as usize {
+    wrr::<T1>(&mut writer, dur1, freq, 0.0, shift * (n as f32));
+    wrr::<T2>(&mut writer, dur2, freq, 0.0, shift * (n as f32));
   }
 }
 
@@ -107,25 +173,31 @@ fn main() {
 
   if let Some(matches) = matches.subcommand_matches("plain") {
     let freq = matches.value_of("FREQ").unwrap().parse::<f32>().expect("Invalid value of FREQ");
-    let dur = matches.value_of("DURATION").unwrap().parse::<u32>().expect("Invalid value of DURATION");
+    let dur = matches.value_of("DURATION").unwrap().parse::<f32>().expect("Invalid value of DURATION");
     let phase = matches.value_of("PHASE").unwrap().parse::<f32>().expect("Invalid value of PHASE");
     let file = matches.value_of("OUTPUT").unwrap();
     match matches.value_of("SHAPE").unwrap() {
-      "sine" => generate_plain(file, freq, dur, phase, sine),
-      "square" => generate_plain(file, freq, dur, phase, square),
-      _ => panic!("Invalid value of SHAPE")
+      "sine" =>
+        write_plain::<Sine>(file, dur, freq, phase),
+      "square" =>
+        write_plain::<Square>(file, dur, freq, phase),
+      _ =>
+        panic!("Invalid value of SHAPE")
     }
   }
   else if let Some(matches) = matches.subcommand_matches("combo") {
     let freq = matches.value_of("FREQ").unwrap().parse::<f32>().expect("Invalid value of FREQ");
-    let dur = matches.value_of("DURATION").unwrap().parse::<u32>().expect("Invalid value of DURATION");
-    let sil = matches.value_of("SILENCE").unwrap().parse::<u32>().expect("Invalid value of SILENCE");
-    let phase = matches.value_of("PHASE").unwrap().parse::<u32>().expect("Invalid value of PHASE");
+    let dur = matches.value_of("DURATION").unwrap().parse::<f32>().expect("Invalid value of DURATION");
+    let sil = matches.value_of("SILENCE").unwrap().parse::<f32>().expect("Invalid value of SILENCE");
+    let phase = matches.value_of("PHASE").unwrap().parse::<f32>().expect("Invalid value of PHASE");
     let file = matches.value_of("OUTPUT").unwrap();
     match matches.value_of("SHAPE").unwrap() {
-      "sine" => generate_combo(file, freq, dur, sil, phase, sine),
-      "square" => generate_combo(file, freq, dur, sil, phase, square),
-      _ => panic!("Invalid value of SHAPE")
+      "sine" =>
+        write_combo::<Sine, Silence>(file, dur, sil, freq, phase),
+      "square" =>
+        write_combo::<Square, Silence>(file, dur, sil, freq, phase),
+      _ =>
+        panic!("Invalid value of SHAPE")
     }
   }
   else {panic!("Invalid subcommand")}
